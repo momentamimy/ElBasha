d1a6ec1a65b9c0057d2487e12611e0eb253c8cf3
momentamimy <momentamimy@gmail.com>
2019-06-10 15:41:48 +0200

    full new design 3

--- app/src/main/java/com/ElBasha/mob/app/SearchActivity.java
+++ app/src/main/java/com/ElBasha/mob/app/SearchActivity.java
@@ -71,7 +71,7 @@ public class SearchActivity extends AppCompatActivity {
         spinnertext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showPopup(v);
+                showPopup(showDialog);
 
             }
         });
